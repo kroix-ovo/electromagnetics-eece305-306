4d71bbfd8eb0f947544a1888163722f28930a5e8
thaleac <thalea.c@gmail.com>
2026-08-26 19:59:21 -0400

    Implement input validation for fromTo function
    Added input validation for Nx3 array requirements.

--- EECE306-Toolbox/+em/+vec/fromTo.m
+++ EECE306-Toolbox/+em/+vec/fromTo.m
@@ -6,7 +6,19 @@ function d = fromTo(P, Q)
 %   Example:
 %       em.vec.fromTo([1 1 1], [2 3 4]) % returns [1 2 3]
 %
-%   See also EM.VEC.MAG, EM.VEC.UNIT.
+%   See also EM.VEC.MAG, EM.VEC.UNIT
+
+    if ~ismatrix(P) || size(P,2) ~= 3
+        error('First input must be an Nx3 array');
+    end
+
+    if ~ismatrix(Q) || size(Q,2) ~= 3
+        error('Second input must be an Nx3 array');
+    end
+
+    if size(P,1) ~= size(Q,1)
+        error('Second input must have the same number of rows as first input');
+    end
 
     d = Q - P;
 end
