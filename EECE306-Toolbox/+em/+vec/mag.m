3e23907ce1f250d6183c29e2886875de4344ce5b
thaleac <thalea.c@gmail.com>
2026-08-26 12:13:53 -0400

    Add function to calculate vector magnitude
    This function calculates the magnitude of a vector, returning an Nx1 array for an Nx3 input array.

---
+++ EECE306-Toolbox/+em/+vec/mag.m
@@ -0,0 +1,11 @@
+function m = mag(A)
+% MAG(A) - The magnitude of a vector
+%       M = MAG(A) returns the Nx1 magnitude array for
+%       the Nx3 input array of vector A
+%
+%   Example:
+%       m = em.vec.mag([3,4,0]) % returns 5
+%   See also EM.VEC.UNIT.
+
+    m = sqrt(sum(A.^2, 2));
+end
