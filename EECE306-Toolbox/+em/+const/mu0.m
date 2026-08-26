0977021f7cd29753d0b823b81a3d928f91217ae7
thaleac <thalea.c@gmail.com>
2026-08-26 12:11:43 -0400

    Add mu0 function for vacuum permeability
    Defines a function to return vacuum permeability.

---
+++ EECE306-Toolbox/+em/+const/mu0.m
@@ -0,0 +1,9 @@
+function m0 = mu0()
+%MU0 - Vacuum permeability
+%   M0 = EM.CONST.MU0() returns the vacuum permeability in H/m
+%
+%   Example:
+%       m0 = em.const.mu0() % returns 1.2566e-06
+
+    m0 = 4 * pi * 1e-7;
+end
