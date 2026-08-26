239cb6839449ef89621860b570e05ec86600f14d
thaleac <thalea.c@gmail.com>
2026-08-26 19:57:18 -0400

    Implement input validation for angle function
    Added input validation for angle function to ensure inputs are Nx3 arrays and have matching row counts.

--- EECE306-Toolbox/+em/+vec/angle.m
+++ EECE306-Toolbox/+em/+vec/angle.m
@@ -6,7 +6,19 @@ function th = angle(A, B)
 %   Example:
 %       em.vec.angle([1 0 0], [0 1 0]) % returns 1.5708 (pi/2)
 %
-%   See also EM.VEC.MAG, EM.VEC.FROMTO.
+%   See also EM.VEC.MAG, EM.VEC.FROMTO
+
+    if ~ismatrix(A) || size(A,2) ~= 3
+        error('First input must be an Nx3 array');
+    end
+
+    if ~ismatrix(B) || size(B,2) ~= 3
+        error('Second input must be an Nx3 array');
+    end
+
+    if size(A,1) ~= size(B,1)
+        error('Second input must have the same number of rows as first input');
+    end
 
     dp = sum(A .* B, 2);
     m = em.vec.mag(A) .* em.vec.mag(B);
