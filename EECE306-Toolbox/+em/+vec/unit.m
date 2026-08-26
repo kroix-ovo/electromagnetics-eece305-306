ca23bebc899b13cc1a1f9fa30c821c8c8d9c4cf1
thaleac <thalea.c@gmail.com>
2026-08-26 19:55:32 -0400

    Enhance unit function with validation and comments
    Updated documentation and added input validation for unit function.

--- EECE306-Toolbox/+em/+vec/unit.m
+++ EECE306-Toolbox/+em/+vec/unit.m
@@ -1,12 +1,16 @@
 function u = unit(A)
-%UNIT Unit vector of each row of A.
+%UNIT Unit vector of each row of A
 %   U = EM.VEC.UNIT(A) returns the Nx3 array of unit vectors of the 
-%   Nx3 input A. A zero row produces a row of NaN.
+%   Nx3 input A. A zero row produces a row of NaN
 %
 %   Example:
 %       em.vec.unit([3 4 0]) % returns [0.6 0.8 0]
 %
-%   See also EM.VEC.MAG.
+%   See also EM.VEC.MAG
+
+    if ~ismatrix(A) || size(A,2) ~= 3
+        error('Input must be an Nx3 array');
+    end
 
     u = A ./ em.vec.mag(A);
 end
