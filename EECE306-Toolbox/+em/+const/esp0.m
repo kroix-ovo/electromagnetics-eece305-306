7619adce566f4c48845110f41b0638ba961d5c20
thaleac <thalea.c@gmail.com>
2026-08-26 12:10:43 -0400

    Create eps0 function for vacuum permittivity
    Added function to return vacuum permittivity.

---
+++ EECE306-Toolbox/+em/+const/esp0.m
@@ -0,0 +1,9 @@
+function e0 = eps0()
+%EPS0 - Vacuum permittivity
+%   E0 = EM.CONST.EPS0() returns the vacuum permittivity in F/m
+%
+%   Example:
+%       e0 = em.const.eps0() % returns 8.8542e-12
+
+e0 = 8.8541878128e-12;
+end
